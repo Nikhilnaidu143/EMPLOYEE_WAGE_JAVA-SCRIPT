26f074711353a17443273739d9e63d444f3060ec
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-27 07:10:57 +0530

    UC-1:- Checked employee is present or not.

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,9 @@
-console.log("WELCOME TO EMPLOYEE WAGE COMPUTATION USING JAVA SCRIPT");
+const IS_ABSENT = 0;
+
+let empCheck = Math.floor(Math.random()*2);
+if(empCheck == IS_ABSENT) {
+   console.log("Employee is absent.");
+}
+else {
+   console.log("Employee is presnt.");
+}
